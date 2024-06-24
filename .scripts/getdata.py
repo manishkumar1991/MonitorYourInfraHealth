dab74bbbf1672c7dea7ee140d354374c1e17538e
Manish Kumar <97503740+manishkumar1991@users.noreply.github.com>
2024-06-24 11:57:33 +0530

    Update getdata.py

--- .scripts/getdata.py
+++ .scripts/getdata.py
@@ -1,1 +1,10 @@
-#adding few lines here
+import sys
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        content = file.read()
+    print(f"Content of {file_path}:\n{content}")
+
+if __name__ == "__main__":
+    file_path = sys.argv[1]
+    read_file(file_path)
