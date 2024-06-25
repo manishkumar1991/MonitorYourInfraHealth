a7b05a916611075123e28651c809e491ae34a559
Manish Kumar <97503740+manishkumar1991@users.noreply.github.com>
2024-06-25 15:29:32 +0530

    Update getdata.py

--- .scripts/getdata.py
+++ .scripts/getdata.py
@@ -17,4 +17,10 @@ if __name__ == "__main__":
     file_path = sys.argv[1]
     read_file(file_path)
     access_token = get_access_token()
-    print(access_token)
+    headers = {
+    "Authorization": f"Bearer {access_token}",
+    "Content-Type": "application/json"
+    }
+    url = "https://management.azure.com/subscriptions/f70efef4-6505-4727-acd8-9d0b3bc0b80e/resourcegroups?api-version=2021-04-01""
+    response = requests.get(url, headers=headers)
+    response.json()
