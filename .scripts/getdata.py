1ad4711fd3de9f49d46f8782525153b689ce4205
Manish Kumar <97503740+manishkumar1991@users.noreply.github.com>
2024-06-25 14:36:05 +0530

    Update getdata.py

--- .scripts/getdata.py
+++ .scripts/getdata.py
@@ -1,10 +1,20 @@
 import sys
+import requests
+from azure.identity import DefaultAzureCredential
+import os
 
 def read_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
     print(f"Content of {file_path}:\n{content}")
+    
+def get_access_token():
+    credential = DefaultAzureCredential()
+    token = credential.get_token('https://management.azure.com/.default')
+    return token.token    
 
 if __name__ == "__main__":
     file_path = sys.argv[1]
     read_file(file_path)
+    access_token = get_access_token()
+    print(access_token)
