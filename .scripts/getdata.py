b42bfebf7a7b5e639e0cb4acd239c6bd5d3db1df
Manish Kumar <97503740+manishkumar1991@users.noreply.github.com>
2024-06-26 17:26:31 +0530

    Update getdata.py

--- .scripts/getdata.py
+++ .scripts/getdata.py
@@ -6,21 +6,9 @@ import os
 def read_file(file_path):
     with open(file_path, 'r') as file:
         content = file.read()
-    print(f"Content of {file_path}:\n{content}")
-    
-def get_access_token():
-    credential = DefaultAzureCredential()
-    token = credential.get_token('https://management.azure.com/.default')
-    return token.token    
+    print(f"Content of {file_path}:\n{content}") 
 
 if __name__ == "__main__":
     file_path = sys.argv[1]
-    read_file(file_path)
-    access_token = get_access_token()
-    headers = {
-    "Authorization": f"Bearer {access_token}",
-    "Content-Type": "application/json"
-    }
-    url = "https://management.azure.com/subscriptions/f70efef4-6505-4727-acd8-9d0b3bc0b80e/resourcegroups?api-version=2021-04-01"
-    response = requests.get(url, headers=headers)
-    print(response.json())
+    if "Schema" in file_path:
+        print(f"Schema file found at {file_path}")
