7e9f844a13fabb5d3e186f43cde40361118fe893
manishkumar1991 <manish.splunk@gmail.com>
2024-06-28 15:07:05 +0530

    test

--- .scripts/tests/asimParsersTest/getdata.py
+++ .scripts/tests/asimParsersTest/getdata.py
@@ -4,10 +4,11 @@ import subprocess
 
 if __name__ == "__main__":
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    GetModifiedFiles = f"git diff --name-only origin/main {current_directory}/../../../Sample%20Data/ASIM/"
+    GetModifiedFiles = f'git diff --name-only origin/main "{current_directory}/../../../Sample Data/ASIM/"'
     try:
         modified_files = subprocess.run(GetModifiedFiles, shell=True, text=True, capture_output=True, check=True)
+        print(modified_files.stdout.splitlines())
     except subprocess.CalledProcessError as e:
         print(f"::error::An error occurred while executing the command: {e}")
         sys.stdout.flush()  # Explicitly flush stdout
-    print(modified_files)
+    
