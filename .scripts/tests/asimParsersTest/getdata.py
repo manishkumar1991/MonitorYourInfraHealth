61109dacf02ed10fffd02f56d381c66c98f0271d
Manish Kumar <97503740+manishkumar1991@users.noreply.github.com>
2024-06-28 14:37:04 +0530

    Update getdata.py

--- .scripts/tests/asimParsersTest/getdata.py
+++ .scripts/tests/asimParsersTest/getdata.py
@@ -4,7 +4,7 @@ import subprocess
 
 if __name__ == "__main__":
     current_directory = os.path.dirname(os.path.abspath(__file__))
-    GetModifiedFiles = f"git diff --name-only origin/main {current_directory}/../../../Sample Data/ASIM/"
+    GetModifiedFiles = f"git diff --name-only origin/main {current_directory}/../../../Sample%20Data/ASIM/"
     try:
         modified_files = subprocess.run(GetModifiedFiles, shell=True, text=True, capture_output=True, check=True)
     except subprocess.CalledProcessError as e:
