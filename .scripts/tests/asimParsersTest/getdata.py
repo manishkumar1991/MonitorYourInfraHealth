a8d80a2b12eb8a1c2119c5647cca838bf78c24eb
Manish Kumar <97503740+manishkumar1991@users.noreply.github.com>
2024-07-10 19:10:27 +0530

    Update getdata.py

--- .scripts/tests/asimParsersTest/getdata.py
+++ .scripts/tests/asimParsersTest/getdata.py
@@ -11,6 +11,7 @@ if __name__ == "__main__":
     GetModifiedFiles = f'git diff --name-only origin/main "{current_directory}/../../../Sample Data/ASIM/"'
     try:
         modified_files = subprocess.run(GetModifiedFiles, shell=True, text=True, capture_output=True, check=True)
+        print(modified_files)
     except subprocess.CalledProcessError as e:
         print(f"::error::An error occurred while executing the command: {e}")
         sys.stdout.flush()  # Explicitly flush stdout
