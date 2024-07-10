0297601a0ab52d4b19d8e3944b7bfa15852cbcbe
Manish Kumar <97503740+manishkumar1991@users.noreply.github.com>
2024-07-10 18:40:09 +0530

    Update getdata.py

--- .scripts/tests/asimParsersTest/getdata.py
+++ .scripts/tests/asimParsersTest/getdata.py
@@ -6,19 +6,18 @@ def custom_sort_key(s):
     return (0, s) if 'Schema' in s else (1, s)
 
 if __name__ == "__main__":
+    prnumber = sys.argv[1]
     current_directory = os.path.dirname(os.path.abspath(__file__))
     GetModifiedFiles = f'git diff --name-only origin/main "{current_directory}/../../../Sample Data/ASIM/"'
     try:
         modified_files = subprocess.run(GetModifiedFiles, shell=True, text=True, capture_output=True, check=True)
-        print(modified_files.stdout.splitlines())
     except subprocess.CalledProcessError as e:
         print(f"::error::An error occurred while executing the command: {e}")
         sys.stdout.flush()  # Explicitly flush stdout
     
-
-
 # Sort the array using the custom sort key
-sorted_array = sorted(modified_files.stdout.splitlines(), key=custom_sort_key)
+    sorted_array = sorted(modified_files.stdout.splitlines(), key=custom_sort_key)
 
 # Print the sorted array
-print(sorted_array)
+    print(sorted_array)
+    print(prnumber)
